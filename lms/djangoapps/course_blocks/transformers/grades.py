f64762f20ff65461bb9cf64bf897cbc73bb49dc6
J. Cliff Dyer <cdyer@edx.org>
2016-05-06 19:50:15 +0000

    fixup: added note on pulling max_score data.

--- lms/djangoapps/course_blocks/transformers/grades.py
+++ lms/djangoapps/course_blocks/transformers/grades.py
@@ -2,34 +2,14 @@
 Grades Transformer
 """
 from openedx.core.lib.block_structure.transformer import BlockStructureTransformer
-from lms.djangoapps.courseware.access_utils import check_start_date
-from xmodule.course_metadata_utils import DEFAULT_START_DATE
-
-from .utils import get_field_on_block
 
 
 class GradesBlockTransformer(BlockStructureTransformer):
     """
-    TODO: Write this.
-
-    From StartDateTransformer:
-    A transformer that enforces the 'start' and 'days_early_for_beta'
-    fields on blocks by removing blocks from the block structure for
-    which the user does not have access. The 'start' field on a
-    block is percolated down to its descendants, so that all blocks
-    enforce the 'start' field from their ancestors.  The assumed
-    'start' value for a block is then the maximum of its parent and its
-    own.
-
-    For a block with multiple parents, the assumed parent start date
-    value is a computed minimum of the start dates of all its parents.
-    So as long as one parent chain allows access, the block has access.
-
-    Staff users are exempted from visibility rules.
     """
     VERSION = 1
 
-    FIELDS_TO_COLLECT = [u'graded', u'max_score', u'weight', u'has_score']
+    FIELDS_TO_COLLECT = [u'graded', u'weight', u'has_score']
 
     @classmethod
     def name(cls):
@@ -46,3 +26,4 @@ class GradesBlockTransformer(BlockStructureTransformer):
         transformer's transform method.
         """
         block_structure.request_xblock_fields(*cls.FIELDS_TO_COLLECT)
+        # get max_score from lms/djangoapps/courseware/grades.py:MaxScoreCache
